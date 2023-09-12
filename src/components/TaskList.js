b68e06ff2a934ec105d4fdb5f61ab4d22fb66d56
IntepeZafer <intepezafer@gmail.com>
2023-09-12 19:02:44 +0300

    ContextFormApp

---
+++ src/components/TaskList.js
@@ -0,0 +1,20 @@
+import TaskShow from './TaskShow';
+import {useContext} from 'react';
+import TasksContext from '../Context/tasks';
+function TaskList() {
+  const {tasks} = useContext(TasksContext)
+  return (
+    <div className="task-list">
+      {tasks.map((task, index) => {
+        return (
+          <TaskShow
+            key={index}
+            task={task}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default TaskList;
